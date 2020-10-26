dca5e3e0d387463e678ebd6d4eb5e8aeacbafaac
Patryk Krajnik <patrykkrajnik@icloud.com>
2020-10-26 15:39:56 +0100

    Rocket Trip

---
+++ Rocket Trip/Game/RockNode.swift
@@ -0,0 +1,13 @@
+//
+//  RockNode.swift
+//  Rocket Trip
+//
+//  Created by Patryk Krajnik on 07/06/2020.
+//  Copyright © 2020 Patryk Krajnik. All rights reserved.
+//
+
+import SpriteKit
+
+class RockNode: SKSpriteNode {
+
+}
